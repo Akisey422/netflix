2ec13b2e6fb461fe590a7da78c7c77768265b93b
yesika <yesika dangol>
2024-05-21 13:19:29 +0545

    minor change

--- index.js
+++ index.js
@@ -26,9 +26,21 @@ app.get("/movie/list", (req, res) => {
 });
 
 //! delete
-// app.delete("/movie.delete", (req, res) => {
-//   return res.status(200).send({});
-// });
+app.delete("/movie.delete", (req, res) => {
+  const movieToBeDeleted = req.body.name;
+  const requiredMovie = movieList.find((item) => {
+    if (item.name === movieToBeDeleted) return item;
+  });
+  if (!requiredMovie)
+    return res.status(404).send({ message: "Movie does ot exist!" });
+});
+const newMovieList = movieList.filter((item, index, array) => {
+  if (item.name !== movieToBeDeleted) return item;
+  movieList = structuredClone(newMovieList);
+
+  //   send response
+  return res.status(200).send({ message: "Movie is deleted successfully," });
+});
 
 //!network port and server
 const PORT = 8001;
